9fbcb5d55cde306fb64def00f3ab90750ea82e55
NukDokPlex <vik.titoff2014@gmail.com>
2021-06-04 03:20:17 +0500

    Убрано перенаправление на форму входа на главной странице.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,7 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     public function index()
     {
